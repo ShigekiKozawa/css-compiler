e092ba5dc9edf4a0b1053a603a3eb8ad9a3bb3b5
kozawa.shigeki <kozawa.shigeki@aainc.co.jp>
2024-04-22 03:02:36 +0900

    add buttons

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,68 +1,118 @@
 (function(){
-  // download button
-  // try {
-  //   let downloadBtn = document.getElementById('download');
-  //   if (!downloadBtn) throw new Error("Download button not found");
-  
-  //   downloadBtn.addEventListener('click', (e) => {
-  //     let textareas = document.querySelectorAll('.contents');
-  //     if (!textareas.length) throw new Error("Textareas not found");
-  
-  //     let textFileContent = '';
-  
-  //     textareas.forEach((textarea, index) => {
-  //       let memoNumber = index + 1;
-  //       textFileContent += `/***** [tab: ${memoNumber}] *****/\n${textarea.value}\n\n`;
-  //     });
-  
-  //     const textBlob = new Blob([textFileContent], { type: 'text/plain' });
-  //     e.currentTarget.href = window.URL.createObjectURL(textBlob);
-  //   });
-  // } catch (error) {
-  //   console.error(`An error occurred: ${error.message}`);
-  // }
-  window.addEventListener('DOMContentLoaded', (event) => {
-    const scssEditor = CodeMirror.fromTextArea(document.getElementById("scss-input"), {
-      lineNumbers: true,
-      mode: "sass",
-      theme: 'vscode-dark',
-      indentUnit: 4,
-      smartIndent: false,
-      extraKeys: {
-        "Shift-Cmd-X": "autocomplete"
-      },
-      matchBrackets: true,
-      autoCloseBrackets: true
-    });
-
-    const cssEditor = CodeMirror.fromTextArea(document.getElementById("css-output"), {
-      lineNumbers: true,
-      mode: "css",
-      theme: 'vscode-dark',
-      readOnly: true
-    });
+  // エディタ
+  const sassEditor = CodeMirror.fromTextArea(document.getElementById("sass-input"), {
+    lineNumbers: true,
+    mode: "sass",
+    theme: 'vscode-dark',
+    indentUnit: 2,
+    tabSize: 2,
+    smartIndent: false,
+    extraKeys: {
+      "Ctrl-Cmd-Z": function(cm) {
+        cm.showHint();
+      }
+    },
+    matchBrackets: true,
+    autoCloseBrackets: true
   });
-  const compileScss = () => {
-      const scssInput = document.getElementById('scss-input').value;
-      console.log(scssInput)
-      Sass.compile(scssInput, { style: Sass.style.COMPACT }, function(result) {
-          if (result.status === 0) {
-              document.getElementById('css-output').value = result.text;
-          } else {
-              alert(`エラーが発生しました: ${result.line}行目`);
-          }
-      });
+  const cssEditor = CodeMirror.fromTextArea(document.getElementById("css-output"), {
+    lineNumbers: true,
+    mode: "css",
+    theme: 'vscode-dark',
+    readOnly: true
+  });
+  // コンパイル
+  const compileSass = () => {
+    const sassInput = sassEditor.getValue();
+
+    Sass.compile(sassInput, { style: Sass.style.compressed }, function(result) {
+      if (result.status === 0) {
+        cssEditor.setValue(result.text);
+      } else {
+        alert(`エラーが発生しました: ${result.line}行目`);
+      }
+    });
   }
+  // ショートカットキー
+  const shortcutKey = () => {
+    document.addEventListener('keydown', function(e) {
+      if (e.ctrlKey && e.metaKey && e.key.toUpperCase() === 'Z') {
+        compileSass();
+        e.preventDefault();
+      }
+    });
+  }
+  // SASS / SCSS / CSSファイルの読み込み
+  const uploadScss = () => {
+    const uploadInput = document.getElementById('js-upload-input');
+    const uploadButton = document.getElementById('js-upload-button');
 
-  const compileButton = document.getElementById('compile-button');
+    uploadButton.addEventListener('click', function() {
+      uploadInput.click();
+    });
+    uploadInput.addEventListener('change', function(e) {
+      const file = e.target.files[0];
+      if (!file) {
+        return;
+      }
+  
+      const reader = new FileReader();
 
-  compileButton.addEventListener('click', compileScss);
-  document.addEventListener('keydown', function(e) {
-    // macOSだと e.metaKey が Command キーになってる、WindowsやLinuxだと e.ctrlKey かな
-    if (e.metaKey && e.shiftKey && e.keyCode == 83) { // keyCode 83 は 'S' のことだ
-      compileScss();
-      e.preventDefault();
-    }
+      reader.onload = (e) => {
+        sassEditor.setValue(e.target.result);
+      }
+      reader.readAsText(file);
+    });
+  }
+  // コピーボタン
+  const copyFunc = () => {
+    const copyButton = document.getElementById('js-copy');
+
+    copyButton.addEventListener('click', async function() {
+      const cssContent = cssEditor.getValue();
+
+      try {
+        if(cssContent.length > 0) {
+          await navigator.clipboard.writeText(cssContent);
+
+          copyButton.classList.add('is-copied');
+          setTimeout(() => {
+            copyButton.classList.remove('is-copied');
+          }, 1500);
+        } else {
+          copyButton.classList.add('is-not-copied');
+          setTimeout(() => {
+            copyButton.classList.remove('is-not-copied');
+          }, 1500);
+        }
+      } catch (err) {
+        console.error(`エラーが発生しました： ${err}`);
+      }
+    });
+  }
+  // ダウンロードボタン
+  const downloadFunc = () => {
+    const downloadButton = document.getElementById('js-download');
+
+    downloadButton.addEventListener('click', function(e) {
+      const cssContent = cssEditor.getValue();
+      const blob = new Blob([cssContent], {type: 'text/css'});
+      const url = window.URL.createObjectURL(blob);
+
+      this.href = url;
+  
+      setTimeout(function() {
+        window.URL.revokeObjectURL(url);
+      }, 100);
+    });
+  }
+  window.addEventListener('DOMContentLoaded', (e) => {
+    const compileButton = document.getElementById('compile-button');
+
+    compileButton.addEventListener('click', compileSass);
+    shortcutKey();
+    uploadScss();
+    copyFunc();
+    downloadFunc();
   });
 }())
-  
